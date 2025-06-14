42b20fb1ebd48fd3db1086b1513ea2b808f229c2
Leeseok-628 <55151494+Leeseok-628@users.noreply.github.com>
2025-06-14 04:12:06 +0000

    Deploying to gh-pages from @ Leeseok-628/Leeseok-628.github.io@5fd01fe9c089da27bc66ba32d2d246fe74548820 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-submenus",
-          title: "submenus",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/_pages/dropdown/";
-          },
-        },{id: "post-a-post-with-plotly-js",
+  },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
